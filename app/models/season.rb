aa9fbc1c7cd9c0e4e03664ac6d6f67a5edd62906
Rick Runyon <rick.runyon@dutchie.com>
2023-01-27 11:51:49 -0500

    A few tables populated

---
+++ app/models/season.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: seasons
+#
+#  id              :bigint           not null, primary key
+#  buy_in          :integer
+#  payouts         :integer          is an Array
+#  year            :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  first_place_id  :bigint
+#  last_place_id   :bigint
+#  second_place_id :bigint
+#  third_place_id  :bigint
+#
+# Indexes
+#
+#  index_seasons_on_first_place_id   (first_place_id)
+#  index_seasons_on_last_place_id    (last_place_id)
+#  index_seasons_on_second_place_id  (second_place_id)
+#  index_seasons_on_third_place_id   (third_place_id)
+#  index_seasons_on_year             (year) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (first_place_id => teams.id)
+#  fk_rails_...  (last_place_id => teams.id)
+#  fk_rails_...  (second_place_id => teams.id)
+#  fk_rails_...  (third_place_id => teams.id)
+#
+class Season < ApplicationRecord
+end
---
+++ app/models/team.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id         :bigint           not null, primary key
+#  avatar_url :string
+#  espn_raw   :jsonb            not null
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  espn_id    :string           not null
+#  season_id  :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_teams_on_season_id              (season_id)
+#  index_teams_on_user_id                (user_id)
+#  index_teams_on_user_id_and_season_id  (user_id,season_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (season_id => seasons.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class Team < ApplicationRecord
+  belongs_to :user
+end
