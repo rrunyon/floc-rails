3574dff04f56e2a9793c34b1acb57fe5dab81f7c
Rick Runyon <rick.runyon@dutchie.com>
2023-01-26 23:30:22 -0500

    Use postgres, initial schema migration

--- db/migrate/20230127040105_create_tables.rb
+++ db/migrate/20230127040105_create_tables.rb
@@ -11,20 +11,6 @@ class CreateTables < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    create_table :seasons do |t|
-      t.year :string, null: false, index: { unique: true }
-
-      t.references :first_place, foreign_key: { to_table: :teams }
-      t.references :second_place, foreign_key: { to_table: :teams }
-      t.references :third_place, foreign_key: { to_table: :teams }
-      t.references :last_place, foreign_key: { to_table: :teams }
-
-      t.integer :buy_in
-      t.integer :payouts, array: true
-
-      t.timestamps
-    end
-
     create_table :teams do |t|
       t.jsonb :espn_raw, null: false
       t.string :espn_id, null: false
@@ -40,12 +26,26 @@ class CreateTables < ActiveRecord::Migration[7.0]
 
     add_index :teams, [:user_id, :season_id], unique: true
 
+    create_table :seasons do |t|
+      t.string :year, null: false, index: { unique: true }
+
+      t.references :first_place, foreign_key: { to_table: :teams }
+      t.references :second_place, foreign_key: { to_table: :teams }
+      t.references :third_place, foreign_key: { to_table: :teams }
+      t.references :last_place, foreign_key: { to_table: :teams }
+
+      t.integer :buy_in
+      t.integer :payouts, array: true
+
+      t.timestamps
+    end
+
     create_table :weeks do |t|
       t.references :season, null: false
       t.integer :week, null: false
       t.boolean :playoff, default: false, null: false
       t.text :recap
-      t.references :recap_author, foreign_key: { to_table: :team }
+      t.references :recap_author, foreign_key: { to_table: :teams }
 
       t.timestamps
     end
@@ -57,8 +57,8 @@ class CreateTables < ActiveRecord::Migration[7.0]
 
       t.references :week
       t.references :season
-      t.references :home_team, foreign_key: { to_table: :team }
-      t.references :away_team, foreign_key: { to_table: :team }
+      t.references :home_team, foreign_key: { to_table: :teams }
+      t.references :away_team, foreign_key: { to_table: :teams }
 
       t.decimal :home_score
       t.decimal :away_score
@@ -67,6 +67,6 @@ class CreateTables < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :matchups, [:week_id, :season_id, :home_team_id, :away_team_id], unique: true
+    add_index :matchups, [:week_id, :season_id, :home_team_id, :away_team_id], unique: true, name: :index_matchups_on_week_season_and_teams
   end
 end
